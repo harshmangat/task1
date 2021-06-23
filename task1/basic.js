8b4f21e7118de1eb595c08685a2e014dbd950c11
Harsh Mangat <harshmangat0007@gmail.com>
2021-06-23 17:39:48 +0530

    stuck

--- task1/basic.js
+++ task1/basic.js
@@ -1,23 +1,23 @@
-const user = [
+let user = [
   {
     input : 'hello',
-    answer : 'Hello'  
+    answer : 'Hello'
   },
   {
     input : 'how are you?',
-    answer :  'I am fine'  
+    answer :  'I am fine'
   },
   {
     input : 'who are you?',
-    answer : 'I am robot'  
+    answer : 'I am robot'
   },
   {
     input : 'how old are you?',
-    answer : 'You just created me'  
+    answer : 'You just created me'
   },
   {
     input : 'who created you?',
-    answer : 'The best JS developer,you!'  
+    answer : 'The best JS developer,you!'
   }
 ]
 
@@ -26,8 +26,12 @@ let userInput = prompt('write your quetion please!');
 for(let i=0; i < user.length; i++){
   if(user[i].input === userInput){
     alert(user[i].answer);
-  } if(!userInput) {
+    break;
+  } else if(!userInput) {
     alert('HELLO WORLD!!!');
     break;
+  
+  }  else {
+    console.log('Your program is not working');
   }
 }
